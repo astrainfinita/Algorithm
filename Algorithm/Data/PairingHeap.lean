3e662d58b42447f40606d83af1ed44a12a659bf9
Yuyang Zhao 赵雨扬 <zhao.yu-yang@foxmail.com>
2026-09-17 14:53:48 +0800

    fix `says`

--- Algorithm/Data/PairingHeap.lean
+++ Algorithm/Data/PairingHeap.lean
@@ -247,7 +247,7 @@ instance [Preorder α] [Std.Total (α := α) (· ≤ ·)] [DecidableRel (α := �
       simp? at this says
         simp only [PairingHeapImp.Heap.toListUnordered_node,
           PairingHeapImp.Heap.toListUnordered_nil, List.append_nil, List.mem_cons,
-          decide_eq_true_eq, forall_eq_or_imp, le_refl, true_and] at this
+          decide_eq_true_eq, forall_eq_or_imp, Std.le_refl, true_and] at this
       simpa [head?, PairingHeapImp.Heap.head?] using this b hb
   toMultiset_tail x h := by
     match x with
