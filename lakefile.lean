8a280767beef6282c522b7067241f976885f02f7
Zhao Yuyang 赵雨扬 <zhao.yu-yang@foxmail.com>
2026-09-17 15:27:32 +0800

    chore: bump toolchain to v4.29.0-rc2 (#107)

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 
 open Lake DSL
 
-require "leanprover-community" / "mathlib" @ git "v4.29.0-rc1"
+require "leanprover-community" / "mathlib" @ git "v4.29.0-rc2"
 
 abbrev algorithmOnlyLinters : Array LeanOption := #[
   ⟨`linter.mathlibStandardSet, true⟩,
